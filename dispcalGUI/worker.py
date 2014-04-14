sdy623/DisplayCAL

commit 01319e378b23feed9526de782b930c9f997c4940

Trivial: To check if the profile loader should be run from 0install, we need to check the name of the parent dir of the module directory
git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1935 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

dispcalGUI/worker.py
@@ -4064,7 +4064,8 @@ class Worker(object):
 					# Check if this is a 0install implementation, in which
 					# case we wanto to call 0launch with the appropriate
 					# command
-					if re.match("sha\d+(?:new)?\W", os.path.basename(pydir)):
+					if re.match("sha\d+(?:new)?\W",
+								os.path.basename(os.apth.dirname(pydir))):
 						executable = ("0launch --command=run-apply-profiles "
 									  "http://%s/0install/dispcalGUI.xml" %
 									  domain.lower())

commit 2a9086feb9879bd95b95e0a4c86274cdb585c9d8

Fix typo introduced in r1935

dispcalGUI/worker.py
@@ -4244,7 +4244,7 @@ class Worker(object):
 					# case we wanto to call 0launch with the appropriate
 					# command
 					if re.match("sha\d+(?:new)?\W",
-								os.path.basename(os.apth.dirname(pydir))):
+								os.path.basename(os.path.dirname(pydir))):
 						executable = ("0launch --command=run-apply-profiles "
 									  "http://%s/0install/dispcalGUI.xml" %
 									  domain.lower())